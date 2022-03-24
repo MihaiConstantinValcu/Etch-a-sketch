7d2efe63d07a1f446a120d27b00de26b75875023
Mihai Valcu <mihai.c.valcu@gmail.com>
2022-03-24 02:13:26 +0200

    finished basic requirements

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
 const container = document.querySelector(".container");
 
-size = 16 * 16;
-for(let i = 0 ; i < size; i++){
-    const cell = document.createElement('div');
-    cell.classList.toggle('cell');
+let width = 400;
+let height = 400;
+let size = 16;
 
-    cell.addEventListener('mouseover',()=>{
-        cell.style.backgroundColor = 'blue';
-    })
+function generateGrid(){
+    let cellHeight = height/size;
+    let cellWidth = width/size;
 
-    container.appendChild(cell);
+    for(let i = 0 ; i < size*size; i++){
+        const cell = document.createElement('div');
+        cell.classList.toggle('cell');
+        cell.style.width = `${cellWidth}px`;
+        cell.style.height = `${cellHeight}px`;
+    
+        cell.addEventListener('mouseover',()=>{
+            console.log(cell.style.backgroundColor)
+            if(cell.style.backgroundColor == ''){
+                let r = Math.floor(Math.random()*255);
+                let g = Math.floor(Math.random()*255);
+                let b = Math.floor(Math.random()*255);
+                cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+            }
+        })
+
+        container.appendChild(cell);
+    }
 }
+
+function deleteGrid(){
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell)=>{
+        container.removeChild(cell);
+    })
+}
+
+function reset(){
+    size = window.prompt("What size should the next grid be?", 16)
+    console.log(size);
+    if(size == '')
+        size = 16;
+    else size = parseInt(size);
+    if(size > 100){
+        size = 99;
+    }
+    deleteGrid();
+    generateGrid();
+}
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener('click',reset);
+
+generateGrid();
